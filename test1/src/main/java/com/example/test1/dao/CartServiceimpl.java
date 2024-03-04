1c3a779e664c8895d2a03f587834044c305c333d
Nomiya623 <omerzhanaliya@gmail.com>
2024-03-04 14:54:51 +0900

    cart update

--- test1/src/main/java/com/example/test1/dao/CartServiceimpl.java
+++ test1/src/main/java/com/example/test1/dao/CartServiceimpl.java
@@ -1,8 +1,17 @@
 package com.example.test1.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class CartServiceimpl implements ProductService{
+public class CartServiceimpl implements CartService{
+
+	@Override
+	public HashMap<String, Object> addAndCheckout(HashMap<String, Object> map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- test1/src/main/java/com/example/test1/model/Cart.java
+++ test1/src/main/java/com/example/test1/model/Cart.java
@@ -1,5 +1,78 @@
 package com.example.test1.model;
 
+import java.util.Date;
+
 public class Cart {
-	
+
+    private int cartNo;
+    private String userId;
+    private String itemNo;
+    private Date cDatetime;
+    private String isPay;
+
+    // Default constructor
+    public Cart() {
+    }
+
+    // Parameterized constructor for all fields
+    public Cart(int cartNo, String userId, String itemNo, Date cDatetime, String isPay) {
+        this.cartNo = cartNo;
+        this.userId = userId;
+        this.itemNo = itemNo;
+        this.cDatetime = cDatetime;
+        this.isPay = isPay;
+    }
+
+    // Getters and Setters
+    public int getCartNo() {
+        return cartNo;
+    }
+
+    public void setCartNo(int cartNo) {
+        this.cartNo = cartNo;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getItemNo() {
+        return itemNo;
+    }
+
+    public void setItemNo(String itemNo) {
+        this.itemNo = itemNo;
+    }
+
+    public Date getCDatetime() {
+        return cDatetime;
+    }
+
+    public void setCDatetime(Date cDatetime) {
+        this.cDatetime = cDatetime;
+    }
+
+    public String getIsPay() {
+        return isPay;
+    }
+
+    public void setIsPay(String isPay) {
+        this.isPay = isPay;
+    }
+
+    // toString method for debugging purposes
+    @Override
+    public String toString() {
+        return "Cart{" +
+                "cartNo=" + cartNo +
+                ", userId='" + userId + '\'' +
+                ", itemNo='" + itemNo + '\'' +
+                ", cDatetime=" + cDatetime +
+                ", isPay='" + isPay + '\'' +
+                '}';
+    }
 }
