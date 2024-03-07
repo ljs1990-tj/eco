bcae2b85f0fd285b04dc9daebb14b7394c1c1322
hommefatale <hommefatale01@naver.com>
2024-03-07 17:16:45 +0900

    2024.03.07

--- test1/src/main/java/com/example/test1/model/ProductFile.java
+++ test1/src/main/java/com/example/test1/model/ProductFile.java
@@ -1,24 +1,5 @@
 package com.example.test1.model;
 
-<<<<<<< HEAD
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import java.util.Date;
-
-@Data
-public class ProductFile {
-	private int itemNo;
-	private String fileNo;
-	private String filePath;
-	private String fileName;
-	private String fileOrgName;
-	private String fileSize;
-	private String fileEtc;
-	private String path;
-}
-=======
 import lombok.Data;
 
 @Data
@@ -32,4 +13,4 @@ public class ProductFile {
 	private String fileEtc;
 	private String path;
 }
->>>>>>> branch 'main' of https://github.com/KDH94/teamProject.git
+
